0b0c77d0f5c229550457a69dca8c672323d0d3ed
Thomas Charles Elswick <finlaymirkwood@gmail.com>
2015-09-03 15:13:58 -0600

    This is just the intial push. Nothing interesting here.

---
+++ Assignment 1/src/SYSTEM.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @author Thomas Elswick
+ * @date 9/3/15
+ * @updated 9/3/15
+ */
+public class SYSTEM 
+{
+	
+	
+
+}
---
+++ Assignment 1/src/Testing.java
@@ -0,0 +1,14 @@
+import java.math.BigInteger;
+
+import javax.xml.bind.annotation.adapters.HexBinaryAdapter;
+
+
+public class Testing {
+
+	public static void main(String[] args) {
+		int test = 0xf;
+		if(test == 0xf) System.out.println(test);
+
+	}
+
+}
